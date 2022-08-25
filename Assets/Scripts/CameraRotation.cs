17bdcaef9145ff232e7db670fbb404afc37f6c44
邱珑 <qiulong@corp.netease.com>
2022-08-25 11:35:27 +0800

    [update][base]初始化

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform lookPoint;
+    [SerializeField]
+    private float rotateSpeed = 12f;
+
+    void Update()
+    {
+        transform.RotateAround(lookPoint.position, Vector3.up, rotateSpeed * Time.deltaTime);
+        transform.LookAt(lookPoint, Vector3.up);
+    }
+}
