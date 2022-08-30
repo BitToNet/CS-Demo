a7cde8f555b79b48989939b3b9fdb757d229f04f
邱珑 <qiulong@corp.netease.com>
2022-08-30 11:41:52 +0800

    [update][模型]删除多余模型

--- Assets/Scripts/Move/MoveController.cs
+++ Assets/Scripts/Move/MoveController.cs
@@ -6,9 +6,7 @@ public class MoveController : MonoBehaviour {
 
 
     public JoyStick joystick;
-
     bool isRun;
-    
     //最大速度
     [SerializeField, Range(0, 100f)]
     float maxSpeed = 10f;
@@ -18,12 +16,26 @@ public class MoveController : MonoBehaviour {
     float currentAcceleration = 10f;
     //速度
     private Vector3 velocity;
-    
     float h, v;
-
     Vector3 moveVec;
-    
     Animator am;
+    
+    //换装
+    public SkinnedMeshRenderer body;
+    public SkinnedMeshRenderer eye;
+    public SkinnedMeshRenderer face;
+    public SkinnedMeshRenderer hair;
+    public SkinnedMeshRenderer hand;
+    public SkinnedMeshRenderer pouch;
+    public Material eyeball1;
+    public Material skin1;
+    public Material ware1;
+    public Material eyeball2;
+    public Material skin2;
+    public Material ware2;
+    public Material eyeball3;
+    public Material skin3;
+    public Material ware3;
 
     // Use this for initialization
     void Start () {
@@ -93,7 +105,9 @@ public class MoveController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
+        ChangeSkin();
         if ((h != 0 || v != 0))
         {
             currentAcceleration = maxAcceleration;
@@ -120,6 +134,38 @@ public class MoveController : MonoBehaviour {
             //moveVec = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * moveVec;
             // nav.Move(moveVec * Time.deltaTime * 5);
             RotatePlayer();
+        }
+    }
+
+    private void ChangeSkin()
+    {
+        // 换肤
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            body.material = ware1;
+            eye.material = eyeball1;
+            face.material = skin1;
+            hair.material = skin1;
+            hand.material = ware1;
+            pouch.material = ware1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            body.material = ware2;
+            eye.material = eyeball2;
+            face.material = skin2;
+            hair.material = skin2;
+            hand.material = ware2;
+            pouch.material = ware2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            body.material = ware3;
+            eye.material = eyeball3;
+            face.material = skin3;
+            hair.material = skin3;
+            hand.material = ware3;
+            pouch.material = ware3;
         }
     }
 
